ded66f568147a75a8d3a87ba748095282cca2e89
Noah Choi <12193696@student.uts.edu.au>
2020-04-01 23:41:35 +1100

    Reduce Redundant Calculation
    + few comments

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -8,38 +8,56 @@
 
 class Calculator {
     static func calculate(_operator: String, value_A: Int, value_B: Int) -> Int {
-        switch MathExtras.Operators(rawValue: _operator) {
+        let result: (partialValue: Int, overflow: Bool)             //tuple which is returned from ~~ReportinOverflow functions
+        switch MathExtras.Operators(rawValue: _operator) {          //
         case .Plus:
-            //if overflow, return Int.min
-            if value_A.addingReportingOverflow(value_B).overflow {return ExpressionValidator.handleError(textToShow: "Overflow detected (Addition)")}
+            //first, calculate ignoring overflow
+            result = value_A.addingReportingOverflow(value_B)
+            //if overflow, emit error
+            if result.overflow {ExpressionValidator.handleError(textToShow: "Overflow detected (Addition)")}
             //else, return properly
-            return value_A+value_B
+            return result.partialValue
         case .Minus:
-            //if overflow, return Int.min
-            if value_A.subtractingReportingOverflow(value_B).overflow {return ExpressionValidator.handleError(textToShow: "Overflow detected (Subtraction)")}
+            //first, calculate ignoring overflow
+            result = value_A.subtractingReportingOverflow(value_B)
+            //if overflow, emit error
+            if result.overflow {ExpressionValidator.handleError(textToShow: "Overflow detected (Subtraction)")}
             //else, return properly
-            return value_A-value_B
+            return result.partialValue
         case .Multiply:
-            //if overflow, return Int.min
-            if value_A.multipliedReportingOverflow(by: value_B).overflow {return ExpressionValidator.handleError(textToShow: "Overflow detected (Multiplication)")}
+            //first, calculate ignoring overflow
+            result = value_A.multipliedReportingOverflow(by: value_B)
+            //if overflow, emit error
+            if result.overflow {ExpressionValidator.handleError(textToShow: "Overflow detected (Multiplication)")}
             //else, return properly
-            return value_A*value_B
+            return result.partialValue
         case .Divide:
-            //if divided by 0, return Int.min
-            if value_B==0 {return ExpressionValidator.handleError(textToShow: "Division by zero was detected (Division operation)")}
-            //else if overflow, return Int.min
-            else if value_A.dividedReportingOverflow(by: value_B).overflow {return ExpressionValidator.handleError(textToShow: "Overflow detected (Division)")}
-            //else, return properly
-            return value_A/value_B
+            //first, if divided by 0, return Int.min
+            if value_B==0 {ExpressionValidator.handleError(textToShow: "Division by zero was detected (Division operation)")}
+            //else
+            else {
+                //calculate ignoring overflow
+                result = value_A.dividedReportingOverflow(by: value_B)
+                //if overflow, emit error
+                if result.overflow {ExpressionValidator.handleError(textToShow: "Overflow detected (Division)")}
+                //else, return properly
+                return value_A/value_B
+            }
         case .Remainder:
-            //if divided by 0, return Int.min
-            if value_B==0 {return ExpressionValidator.handleError(textToShow: "Division by zero was detected (Remainder operation)")}
-            //else if overflow, return Int.min
-            else if value_A.remainderReportingOverflow(dividingBy: value_B).overflow {return ExpressionValidator.handleError(textToShow: "Overflow detected (Remainder)")}
-            //else, return properly
-            return value_A%value_B
+            //first, if divided by 0, return Int.min
+            if value_B==0 {ExpressionValidator.handleError(textToShow: "Division by zero was detected (Remainder operation)")}
+            //else
+            else{
+                //calculate ignoring overflow
+                result = value_A.remainderReportingOverflow(dividingBy: value_B)
+                //if overflow, emit error
+                if result.overflow {ExpressionValidator.handleError(textToShow: "Overflow detected (Remainder)")}
+                //else, return properly
+                return value_A%value_B
+            }
         default:
-            return ExpressionValidator.handleError(textToShow: "Internal Error: Operator identification Failed at calculation stage")
+            ExpressionValidator.handleError(textToShow: "Internal Error: Operator identification Failed at calculation stage")
         }
+        return Int.min
     }
 }
--- calc/ExpressionValidator.swift
+++ calc/ExpressionValidator.swift
@@ -9,28 +9,24 @@
 import Foundation
 
 class ExpressionValidator {
-    let ERROR: String = "Invalid Input: ";
+    let ERROR: String = "Invalid Input: ";                  //for my convenience
     
-    func even(number: Int) -> Bool {
+    func even(number: Int) -> Bool {                        //function which returns true if given number is an even number
         if number%2 == 0 {return true}
         return false
     }
 
-    func validate(original: [String]) -> [String]{
-        if original.isEmpty {                               //if given arguments were actually nothing
-            print(ERROR+"No arguments received")
-            exit(-1)
+    func validate(original: [String]) -> [String]{          //function to initially check for an obvious mal-inputs
+        if original.isEmpty {                                   //if given arguments were actually nothing
+            ExpressionValidator.handleError(textToShow: ERROR+"No arguments received")
         }
-        else if even(number: original.count) {              //if given argumnets' number is even (supposedly odd if valid)
-            print(ERROR+"Insufficient number of arguments")
-            exit(-1)
+        else if even(number: original.count) {                  //if given argumnets' number is even (supposedly odd if valid)
+            ExpressionValidator.handleError(textToShow: ERROR+"Insufficient number of arguments")
         }
-        else {
-            return original
-        }
+        return original
     }
     
-    static func handleError(textToShow: String) -> Int{
+    static func handleError(textToShow: String){
         print(textToShow)
         exit(-1)
     }
